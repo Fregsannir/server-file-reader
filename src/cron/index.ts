b4c1876af0988bca7af7cee837b8aa7747631f7d
Teimuraz Kozanashvili <kozanashvili.teimuraz@gmail.com>
2022-02-24 20:35:00 +0300

    Refactor with map

--- src/cron/index.ts
+++ src/cron/index.ts
@@ -3,7 +3,7 @@ import { cacheMiddleware } from "../cache/index";
 
 import cron = require("node-cron");
 
-export const schedule = cron.schedule("* */1 * * * *", async () => {
+export const schedule = cron.schedule("* */2 * * * *", async () => {
     const cacheKeys: string[] = await cacheMiddleware.cache.store.keys();
     console.log(`Cache Keys length is ${cacheKeys.length}`);
 
@@ -26,22 +26,24 @@ export const schedule = cron.schedule("* */1 * * * *", async () => {
                 )
                 .then((res) => {
                     if (res.data?.orderCode && status === "order_complete") {
-                        axios
-                            .post(
-                                `${process.env.MAIN_SERVER_PROTOCOL}://${process.env.MAIN_SERVER_HOST}/landing/ticket/send`,
-                                {
-                                    orderCode: cacheKey,
-                                },
-                                {
-                                    headers: {
-                                        Origin: "https://landing-api.flashback.one",
+                        setTimeout(() => {
+                            axios
+                                .post(
+                                    `${process.env.MAIN_SERVER_PROTOCOL}://${process.env.MAIN_SERVER_HOST}/landing/ticket/send`,
+                                    {
+                                        orderCode: cacheKey,
                                     },
-                                }
-                            )
-                            .then((res) => {
-                                console.log(res);
-                            })
-                            .catch((e) => console.error(e));
+                                    {
+                                        headers: {
+                                            Origin: "https://landing-api.flashback.one",
+                                        },
+                                    }
+                                )
+                                .then((res) => {
+                                    console.log(res);
+                                })
+                                .catch((e) => console.error(e));
+                        }, 10000);
                     }
                 })
                 .catch((e) => console.error(e));

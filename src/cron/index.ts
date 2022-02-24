c29bb119a0c98e3cf787702054dc8bc6e6887f7b
Fregsannir <kozanashvili.teimuraz@gmail.com>
2022-02-24 16:10:35 +0100

    Cron axios host change

--- src/cron/index.ts
+++ src/cron/index.ts
@@ -19,7 +19,7 @@ export class Cron {
 
                           const response = (
                               await axios.post(
-                                  `${process.env.SERVER_PROTOCOL}://${process.env.SERVER_HOST}/landing/status`,
+                                  `${process.env.MAIN_SERVER_PROTOCOL}://${process.env.MAIN_SERVER_HOST}/landing/status`,
                                   {
                                       orderCode: cacheKey,
                                       status: await cacheMiddleware.cache.store.get(
@@ -31,7 +31,7 @@ export class Cron {
 
                           if (response.orderCode && status === "order_complete") {
                               await axios.post(
-                                  `${process.env.SERVER_PROTOCOL}://${process.env.SERVER_HOST}/landing/ticket/send`,
+                                  `${process.env.MAIN_SERVER_PROTOCOL}://${process.env.MAIN_SERVER_HOST}/landing/ticket/send`,
                                   {
                                       orderCode: cacheKey,
                                   }
